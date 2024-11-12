bf8ff186af57648192ce57650d824fad140979c9
ANGELO GABRIEL WONG GUTIERREZ <awong@edu.upolitecnica.cr>
2024-11-12 11:51:57 -0600

    gMerge pull request #1 from AngeloWong09/Caster009-Usuarios-1
    MVC con Controlador y modelo de los usuariosMerge pull request #1 from AngeloWong09/Caster009-Usuarios-1
    
    MVC con Controlador y modelo de los usuariosMerge pull request #1 from AngeloWong09/Caster009-Usuarios-1
    
    MVC con Controlador y modelo de los usuarios

--- Gimnasio/Gimnasio/Controllers/HomeController.cs
+++ Gimnasio/Gimnasio/Controllers/HomeController.cs
@@ -30,3 +30,4 @@ namespace Gimnasio.Controllers
         }
     }
 }
+//Aquig
--- Gimnasio/Gimnasio/obj/Debug/net8.0/Gimnasio.AssemblyInfo.cs
+++ Gimnasio/Gimnasio/obj/Debug/net8.0/Gimnasio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gimnasio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77d5801b6777042b62f41c7cdf43f9355cb667c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gimnasio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gimnasio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
